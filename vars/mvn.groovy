a548d25d73d90158524ac3134f29821dd6245444
신현일/클라우드개발센터/NE <hyunil.shin@nhnent.com>
2019-09-26 15:11:45 +0900

    Update mvn.groovy

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,22 +1,27 @@
-node {
-    checkout scm
-  
-    def testType
-    stage('input') {
-      def userInput = input message: 'Choose test type', parameters: [string(defaultValue: 'test', description: 'test or verify', name: 'testType', trim: false)], submitterParameter: 'store'
-      testType = userInput.testType
-    }
+#!/usr/bin/env groovy
 
-    stage('build') {
-        withEnv(["PATH+MAVEN=${tool 'apache-maven-3.6.0'}/bin"]) {
-            sh 'mvn --version'
-            sh "mvn clean ${testType}"
+def call() {
+    node {
+        checkout scm
+
+        def testType
+        stage('input') {
+          def userInput = input message: 'Choose test type', parameters: [string(defaultValue: 'test', description: 'test or verify', name: 'testType', trim: false)], submitterParameter: 'store'
+          testType = userInput.testType
+        }
+
+        stage('build') {
+            withEnv(["PATH+MAVEN=${tool 'apache-maven-3.6.0'}/bin"]) {
+                sh 'mvn --version'
+                sh "mvn clean ${testType}"
+            }
+        }
+
+        stage('report') {
+            junit 'target/surefire-reports/*.xml'
+            jacoco execPattern: 'target/**.exec'
+            addShortText background: '', borderColor: '', color: '', link: '', text: testType
         }
     }
-    
-    stage('report') {
-        junit 'target/surefire-reports/*.xml'
-        jacoco execPattern: 'target/**.exec'
-        addShortText background: '', borderColor: '', color: '', link: '', text: testType
-    }
+
 }
