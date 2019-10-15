22333a77d9af7688d6399994ccb0bd6881ffbeb0
신현일/클라우드개발센터/NE <hyunil.shin@nhnent.com>
2019-10-15 11:37:12 +0900

    Update Zoo.groovy

--- src/ci/Zoo.groovy
+++ src/ci/Zoo.groovy
@@ -5,8 +5,7 @@ def abc(param1, param2) {
   node('master') {
     sh "echo parameters: ${param1}, ${param2}"
     sh "echo jenkins parameters: ${params.env}"
-    
-    def m = load('module.groovy')
-    m.listOptions([param1, param2])   
+        
+    Util.listOptions([param1, param2])   
   }
 }
