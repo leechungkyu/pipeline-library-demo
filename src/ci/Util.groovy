cd475e0acbb7147a74654bbd225abc1e4892d1cd
신현일/클라우드개발센터/NE <hyunil.shin@nhnent.com>
2019-10-15 12:01:31 +0900

    Update Util.groovy

--- src/ci/Util.groovy
+++ src/ci/Util.groovy
@@ -2,8 +2,9 @@
 package ci;
 
 static def printOptions(options = []) {
+  // do not use echo: MissingMethodException
   println "this is module"
   for(int i = 0; i < options.size(); i++) {
-    echo options[i]
+    println options[i]
   }
 }
